fad0f2aecd3102a5f48a340d9a8f2f4f9cfb5711
Anna Baranek <annaxbaranek@gmail.com>
2024-05-18 15:05:32 +0200

    data fetched

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,77 +1,99 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 
-
-
-export const CityOptions = ({cities}) => { 
-  console.log(cities)
-  return (
-  <>
-  <option value="">Vyberte</option>
-  {cities.map((city) => {
+export const CityOptions = ({ cities }) => {
   return (
     <>
-     <option key={city.code} value={city.code} >{city.name}</option>;
+      <option value="">Vyberte</option>
+      {cities.map((city) => {
+        console.log(city.code);
+        return (
+          <option key={city.code} value={city.code}>
+            {city.name}
+          </option>
+        );
+      })}
     </>
-  )
-})}
-  </>
   );
-}
+};
 
-
+export const DatesOptions = ({ dates }) => {
+  console.log(dates);
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {dates.map((date) => {
+        console.log(date.dateBasic);
+        return (
+          <option key={date.dateBasic} value={date.dateBasic}>
+            {date.dateCs}
+          </option>
+        );
+      })}
+    </>
+  );
+};
 
 export const JourneyPicker = ({ onJourneyChange }) => {
   const [fromCity, setFromCity] = useState("");
   const [toCity, setToCity] = useState("");
   const [date, setDate] = useState("");
-  const [cities, setCities] = useState([
-    { name: 'Praha', code: 'CZ-PRG' },
-    { name: 'Brno', code: 'CZ-BRQ' },
-  ]);
-  
+  const [cities, setCities] = useState([]);
+  const [dates, setDates] = useState([]);
 
-  const HandleSubmit = (event) => {
+  useEffect(() => {
+    const fetchCities = async () => {
+      const response = await fetch(
+        "https://apps.kodim.cz/daweb/leviexpress/api/cities"
+      );
+      const data = await response.json();
+      setCities(data.results);
+    };
+    fetchCities();
+
+    const fetchDates = async () => {
+      const response = await fetch(
+        "https://apps.kodim.cz/daweb/leviexpress/api/dates"
+      );
+      const data = await response.json();
+      setDates(data.results);
+    };
+    fetchDates();
+
+  }, []);
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Odesílám formulář s cestou...");
-    console.log(fromCity);
-    console.log(toCity);
-    console.log(date);
+    //TODO
+    console.log(
+      `Odesílám formulář s cestou...From: ${fromCity} To: ${toCity} Date: ${date} `
+    );
   };
 
   return (
     <div className="journey-picker container">
       <h2 className="journey-picker__head">Kam chcete jet?</h2>
       <div className="journey-picker__body">
-        <form onSubmit={HandleSubmit} className="journey-picker__form">
+        <form onSubmit={handleSubmit} className="journey-picker__form">
           <label>
             <div className="journey-picker__label">Odkud:</div>
             <select
               value={fromCity}
               onChange={(e) => setFromCity(e.target.value)}
             >
-         <CityOptions cities={cities} />
-  
-
+              <CityOptions cities={cities} />
             </select>
           </label>
           <label>
             <div className="journey-picker__label">Kam:</div>
-            <select  value={toCity}
-              onChange={(e) => setToCity(e.target.value)}>
-         <CityOptions cities={cities}/>
+            <select value={toCity} onChange={(e) => setToCity(e.target.value)}>
+              <CityOptions cities={cities} />
             </select>
           </label>
           <label>
             <div className="journey-picker__label">Datum:</div>
-            <select  value={date}
-              onChange={(e) => setDate(e.target.value)}>
-              <option value="">Vyberte</option>
-              <option value="datum01">Datum 01</option>
-              <option value="datum02">Datum 02</option>
-              <option value="datum03">Datum 03</option>
-              <option value="datum04">Datum 04</option>
-              <option value="datum05">Datum 05</option>
+            <select value={date} onChange={(e) => setDate(e.target.value)}>
+              <DatesOptions dates={dates} />
             </select>
           </label>
           <div className="journey-picker__controls">
